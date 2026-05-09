75d0209080fc07f6bd0cb1d22d0a4df240ef03c5
Your Name <dawitworkujima@gmail.com>
2026-05-09 13:37:21 +0300

    fix n the chatbot

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -85,6 +85,8 @@ app.use(
 app.use(helmet({
   contentSecurityPolicy: false,
   crossOriginEmbedderPolicy: false,
+  crossOriginResourcePolicy: false,
+  crossOriginOpenerPolicy: false,
 }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
